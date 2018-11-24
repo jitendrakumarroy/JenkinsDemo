a1faa65baee2422424f50f04405da568ed5e8f14
jitendraroy <jitendrakumar204@gmail.com>
2018-11-24 19:24:10 +0530

    testing 2

--- src/JenkinsDemo/Jenkinsdemo.java
+++ src/JenkinsDemo/Jenkinsdemo.java
@@ -21,7 +21,7 @@ public class Jenkinsdemo {
 		driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("Jitendra@26");
 		driver.findElement(By.xpath("//input[@type='submit']")).click();
 		System.out.println(driver.getCurrentUrl());
-		
+		driver.close();
 	
 
 	}
